a186206dd96a731a206d2c6e922acd06c3d3838d
strobelightprojects <122390694+strobelightprojects@users.noreply.github.com>
2025-12-10 17:50:02 -0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
-import 'screens/inventory_screen.dart'; // Use the new dedicated screen
-
-// --- Configuration ---
-// Removed baseUrl definition here; it is now in api_service.dart
+import 'screens/inventory_screen.dart'; 
+import 'screens/order_list_screen.dart'; // Import the new screen
 
 void main() {
   runApp(const InventoryApp());
@@ -20,8 +18,59 @@ class InventoryApp extends StatelessWidget {
         visualDensity: VisualDensity.adaptivePlatformDensity,
         useMaterial3: true,
       ),
-      // The home is now the InventoryScreen
-      home: const InventoryScreen(), 
+      // Set the home to the new MainNavigator widget
+      home: const MainNavigator(), 
+    );
+  }
+}
+
+// New Widget to handle bottom navigation
+class MainNavigator extends StatefulWidget {
+  const MainNavigator({super.key});
+
+  @override
+  State<MainNavigator> createState() => _MainNavigatorState();
+}
+
+class _MainNavigatorState extends State<MainNavigator> {
+  int _selectedIndex = 0;
+
+  // List of screens for the navigation bar
+  static const List<Widget> _screens = <Widget>[
+    InventoryScreen(), // Index 0: Plant Inventory
+    OrderListScreen(),  // Index 1: Customer Orders
+    // TODO: Add SupplierListScreen here at Index 2
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // The current screen is displayed here
+      body: _screens.elementAt(_selectedIndex), 
+      
+      // Bottom Navigation Bar for switching views
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.grass),
+            label: 'Inventory',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.receipt),
+            label: 'Orders',
+          ),
+          // TODO: Add Suppliers Item here
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.green[800],
+        onTap: _onItemTapped,
+      ),
     );
   }
 }
